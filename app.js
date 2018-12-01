08e5f587f234f964bee1ea27775014e53333278e
unknown <smp.sie@gmail.com>
2018-12-01 10:29:11 +0000

    Setup done

---
+++ app.js
@@ -0,0 +1,45 @@
+// Express
+var express = require('express');
+var app = express();
+const port = 80;
+
+// Path
+var path = require("path")
+
+// Express Handlebars
+var exphbs = require("express-handlebars");
+app.set('views', path.join(__dirname, 'views'))
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// MongoDB & Mongoose
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/loginapp', {useNewUrlParser: true})
+var db = mongoose.connection;
+
+// Body Parser
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Express Sessions
+var session = require('express-session')
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false
+}));
+
+// Keys
+var keys = require('./keys/secrets')
+
+// Main Page
+app.get('/', (req,res) => {
+	res.send(keys.cookieSecret);
+})
+
+// Set static folder
+app.use(express.static(path.join(__dirname,'public')))
+
+// Set port
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
